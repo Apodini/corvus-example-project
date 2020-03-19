ea3457dff02dcb1b8b34f1cd505c52a87e306e40
Berzan Yildiz <berzanmikaili@gmail.com>
2020-03-19 13:32:10 +0100

    Documentation and Code cleanup for #5 SoftDelete and Restore

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(
             url: "https://github.com/Apodini/corvus",
-            from: "0.0.5"
+            from: "0.0.6"
         ),
 
         .package(
